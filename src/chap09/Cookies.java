0f89e6b3c8efc381c2d062d10d8bb296f8b08cb8
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-04-12 11:35:32 +0900

    learn cookie

--- src/chap09/Cookies.java
+++ src/chap09/Cookies.java
@@ -1,6 +1,7 @@
 package chap09;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 
 // 책 p222
 public class Cookies {
-	private Map<String, Cookies> cookieMap = new HashMap<>();
+	private Map<String, Cookie> cookieMap = new HashMap<>();
 	
 	public Cookies(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
@@ -22,11 +23,33 @@ public class Cookies {
 		}
 	}
 	
+	public Cookie getCookie(String name) {
+		return cookieMap.get(name);
+	}
+	
+	public String getValue(String name) throws IOException {
+		Cookie cookie = cookieMap.get(name);
+		
+		if (cookie == null) {
+			return null;
+		}
+		
+		return URLDecoder.decode(cookie.getValue(), "utf-8");
+	}
+	
 	public boolean exists(String name) {
 		return cookieMap.get(name) != null;
 	}
 	
 	public static Cookie createCookie(String name, String value) throws IOException {
 		return new Cookie(name, URLEncoder.encode(value, "utf-8"));
+	}
+	
+	public static Cookie createCookie(String name, String value, String path, int maxAge) throws IOException {
+		Cookie cookie = new Cookie(name, URLEncoder.encode(value, "utf-8"));
+		cookie.setMaxAge(maxAge);
+		cookie.setPath(path);
+		
+		return cookie;
 	}
 }
