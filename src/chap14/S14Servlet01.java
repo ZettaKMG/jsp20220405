76a512c30fe9e58c9af8eeecd5e3a4da7a70c688
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-04-21 16:48:21 +0900

    learn sql, jdbc

--- src/chap14/S14Servlet01.java
+++ src/chap14/S14Servlet01.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -34,6 +36,7 @@ public class S14Servlet01 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		ServletContext application = getServletContext();
+		List<String> cities = new ArrayList<>();
 		
 		// database에서 records 가져오기
 		// 1. 연결설정
@@ -50,7 +53,8 @@ public class S14Servlet01 extends HttpServlet {
 			// 4. 실행결과 정제
 			while (rs.next()) {
 				String city = rs.getString(1);
-				System.out.println(city);
+//				System.out.println(city);
+				cities.add(city);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -69,7 +73,7 @@ public class S14Servlet01 extends HttpServlet {
 		*/
 		
 		// request에 records 넣기
-//		request.setAttribute("cities", cities);
+		request.setAttribute("cities", cities);
 		
 		// jsp로 forward
 		String location = "/WEB-INF/view/chap14/ex01.jsp";
