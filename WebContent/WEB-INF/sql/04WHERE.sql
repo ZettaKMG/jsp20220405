07f06d36acd2d5104d5418d07b6eb0a2c322f7b1
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-04-22 10:31:48 +0900

    learn where

---
+++ WebContent/WEB-INF/sql/04WHERE.sql
@@ -0,0 +1,24 @@
+USE w3schools;
+
+SELECT * FROM Employees;
+SELECT FirstName FROM Employees;
+SELECT CustomerName, Address FROM Customers;
+SELECT Address, CustomerName FROM Customers;
+
+-- WHERE : 어떤 Row(RECORD)를 선택할 지 결정하는 키워드
+
+-- 예제1) 나라가 멕시코인 고객들 조회
+SELECT * FROM Customers
+WHERE Country = 'Mexico';
+
+-- 예제2) 나라가 독일인 고객들 조회
+SELECT * FROM Customers
+WHERE Country = 'Germany';
+
+-- 작은 따옴표 : 문자열값
+
+-- 예제3) 고객 ID가 1번인 고객조회
+SELECT * FROM Customers WHERE CustomerID = 1; -- 숫자 형식은 작은 따옴표 안씀 (다만, 써도 상관없음)
+SELECT * FROM Customers WHERE CustomerID = 90;
+SELECT * FROM Customers WHERE CustomerID = '1';
+SELECT * FROM Customers WHERE Country = Germany; -- 문자형식은 작은 따옴표를 꼭 써야 함
