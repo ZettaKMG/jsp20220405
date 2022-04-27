d885b5d71ff3fc49f2558d9b8918b3caf231b5aa
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-04-27 12:37:53 +0900

    learn between

---
+++ WebContent/WEB-INF/sql/19BETWEEN.sql
@@ -0,0 +1,10 @@
+USE w3schools;
+
+SELECT * FROM Employees ORDER BY BirthDate;
+
+SELECT * FROM Employees
+WHERE BirthDate >= '1960-01-01'
+  AND BirthDate <= '1969-12-31';
+  
+SELECT * FROM Employees
+WHERE BirthDate BETWEEN '1960-01-01' AND '1969-12-31';
