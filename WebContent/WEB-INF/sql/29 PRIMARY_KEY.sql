8876cf838705ece4761708defa610b3b6672d2cd
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-04-28 12:28:13 +0900

    learn primary key (= not null + unique), auto increment

--- WebContent/WEB-INF/sql/29 PRIMARY_KEY.sql
+++ WebContent/WEB-INF/sql/29 PRIMARY_KEY.sql
@@ -18,3 +18,13 @@ INSERT INTO MyTable13 (col1, col2) VALUES ('a', 'b');
 INSERT INTO MyTable13 (col1, col2) VALUES ('a', 'b'); -- UNIQUE 제약사항
 INSERT INTO MyTable13 (col1, col2) VALUES ('a', null); -- NOT NULL 제약사항
 SELECT * FROM MyTable13;
+
+-- AUTO_INCREMENT : 자동으로 값 증가
+CREATE TABLE MyTable18 (
+	col1 INT PRIMARY KEY AUTO_INCREMENT, -- 1부터 자동으로 1씩 증가하는 값 입력해줌
+    col2 VARCHAR(10)
+);
+SELECT * FROM MyTable18;
+INSERT INTO MyTable18 (col2) VALUES('abce');
+
+DELETE FROM MyTable18 WHERE col1 = 10;
